46da139028327265b7338335a33672f5865c4770
王小布 <wangxiaobu01@foxmail.com>
2022-03-31 18:04:48 +0800

    完成20天的学习

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,62 @@
 <template>
   <div class="login_container">
     <div class="login_box">
+      <!-- 头像区 -->
       <div class="avatar_box">
         <img src="../assets/logo.png" alt="">
       </div>
+      <!-- 登录表单区 -->
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+        </el-form-item>
+        <!-- 按钮区 -->
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      // 登录表单的数据绑定对象
+      loginForm: {
+        username: 'zhang',
+        password: '123'
+      },
+      //表单的验证规则对象
+      loginFormRules: {
+        //验证用户名是否合法
+        username:[
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        //验证密码是否合法
+        password:[
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    //点击重置按钮，重置登录表单
+    resetLoginForm() {
+      // console.log(this);
+      this.$refs.loginFormRef.resetFields();
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
@@ -29,7 +76,7 @@ export default {};
   top: 50%;
   transform: translate(-50%);
 
-  .avatar_box{
+.avatar_box{
     height: 130px;
     width: 130px;
     border: 1px solid #eee;
@@ -47,5 +94,18 @@ export default {};
       background-color: #eee;
     }
   }
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
 </style>
