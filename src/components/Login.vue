8c184144dc6ef835e8ad051f30099aecef55ea25
王小布 <wangxiaobu01@foxmail.com>
2022-04-01 14:33:19 +0800

    完成登录24课

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -17,7 +17,7 @@
         </el-form-item>
         <!-- 按钮区 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
           <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
@@ -31,8 +31,8 @@ export default {
     return {
       // 登录表单的数据绑定对象
       loginForm: {
-        username: 'zhang',
-        password: '123'
+        username: 'admin',
+        password: '123456'
       },
       //表单的验证规则对象
       loginFormRules: {
@@ -54,6 +54,30 @@ export default {
     resetLoginForm() {
       // console.log(this);
       this.$refs.loginFormRef.resetFields();
+    },
+    login() {
+      //跟下边 awak 一起的 async
+      this.$refs.loginFormRef.validate(async valid=>{
+        console.log(valid);
+        //这两行 是 没有后端服务，所以直接跳转home组件
+        // this.$router.push('/home')
+        // return
+        if(!valid) return;
+        // 知识点awak
+        // const result = await this.$http.post('login', this.loginForm)
+        const {data:res} = await this.$http.post('login', this.loginForm);
+        console(res);
+        if (res.meta.status != 200) return this.$message.error('登录失败');
+        console.log('登录成功');
+        this.$message.success('登录成功');
+        //1. 将登录成功之后的token，保存到客户端的sessionStorage中
+        //1.1 项目中除了登录之外的其他API接口，必须在登录之后才能访问
+        //1.2 tonken只应在当前网站打开期间生效，所以将token保存在sessionStorage中
+        console.log(res)
+        window.sessionStorage.setItem('token', res.data.token);
+        //2. 通过编程式导航跳转到后台主页，路由地址是/home
+        this.$router.push('/home')
+      })
     }
   }
 };
@@ -108,4 +132,6 @@ export default {
   padding: 0 20px;
   box-sizing: border-box;
 }
+
+
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import { Button } from 'element-ui'
 import {Form, FormItem} from 'element-ui'
 import {Input} from 'element-ui'
+//导入 弹框提示组件
+import {Message} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+//配置方式和其他不同，需要全局挂载
+Vue.prototype.$message = Message
