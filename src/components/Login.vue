cbe2fe48985c37a73d9df99c815f641a986f0bb7
王小布 <wangxiaobu01@foxmail.com>
2022-04-03 09:06:54 +0800

    完成27节课，语法配置

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,17 +3,30 @@
     <div class="login_box">
       <!-- 头像区 -->
       <div class="avatar_box">
-        <img src="../assets/logo.png" alt="">
+        <img src="../assets/logo.png" alt="" />
       </div>
       <!-- 登录表单区 -->
-      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
         <!-- 用户名 -->
         <el-form-item prop="username">
-          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          ></el-input>
         </el-form-item>
         <!-- 密码 -->
         <el-form-item prop="password">
-          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            type="password"
+          ></el-input>
         </el-form-item>
         <!-- 按钮区 -->
         <el-form-item class="btns">
@@ -32,55 +45,60 @@ export default {
       // 登录表单的数据绑定对象
       loginForm: {
         username: 'admin',
-        password: '123456'
+        password: '123456',
       },
       //表单的验证规则对象
       loginFormRules: {
         //验证用户名是否合法
-        username:[
+        username: [
           { required: true, message: '请输入登录名称', trigger: 'blur' },
-          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+          {
+            min: 3,
+            max: 10,
+            message: '长度在 3 到 10 个字符',
+            trigger: 'blur',
+          },
         ],
         //验证密码是否合法
-        password:[
+        password: [
           { required: true, message: '请输入密码', trigger: 'blur' },
-          { min: 6, max: 15, message: '长度在 6到 15 个字符', trigger: 'blur' }
-        ]
-      }
-    };
+          { min: 6, max: 15, message: '长度在 6到 15 个字符', trigger: 'blur' },
+        ],
+      },
+    }
   },
   methods: {
     //点击重置按钮，重置登录表单
     resetLoginForm() {
       // console.log(this);
-      this.$refs.loginFormRef.resetFields();
+      this.$refs.loginFormRef.resetFields()
     },
     login() {
       //跟下边 awak 一起的 async
-      this.$refs.loginFormRef.validate(async valid=>{
-        console.log(valid);
+      this.$refs.loginFormRef.validate(async (valid) => {
+        console.log(valid)
         //这两行 是 没有后端服务，所以直接跳转home组件
         // this.$router.push('/home')
         // return
-        if(!valid) return;
+        if (!valid) return
         // 知识点awak
         // const result = await this.$http.post('login', this.loginForm)
-        const {data:res} = await this.$http.post('login', this.loginForm);
-        console(res);
-        if (res.meta.status != 200) return this.$message.error('登录失败');
-        console.log('登录成功');
-        this.$message.success('登录成功');
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        console(res)
+        if (res.meta.status != 200) return this.$message.error('登录失败')
+        console.log('登录成功')
+        this.$message.success('登录成功')
         //1. 将登录成功之后的token，保存到客户端的sessionStorage中
         //1.1 项目中除了登录之外的其他API接口，必须在登录之后才能访问
         //1.2 tonken只应在当前网站打开期间生效，所以将token保存在sessionStorage中
         console.log(res)
-        window.sessionStorage.setItem('token', res.data.token);
+        window.sessionStorage.setItem('token', res.data.token)
         //2. 通过编程式导航跳转到后台主页，路由地址是/home
         this.$router.push('/home')
       })
-    }
-  }
-};
+    },
+  },
+}
 </script>
 
 <style lang="less" scoped>
@@ -90,7 +108,7 @@ export default {
   height: 100%;
 }
 
-.login_box{
+.login_box {
   width: 450px;
   height: 300px;
   background-color: #fff;
@@ -100,7 +118,7 @@ export default {
   top: 50%;
   transform: translate(-50%);
 
-.avatar_box{
+  .avatar_box {
     height: 130px;
     width: 130px;
     border: 1px solid #eee;
@@ -111,7 +129,7 @@ export default {
     left: 50%;
     transform: translate(-50%, -50%);
     background-color: #fff;
-    img{
+    img {
       width: 100%;
       height: 100%;
       border-radius: 50%;
@@ -132,6 +150,4 @@ export default {
   padding: 0 20px;
   box-sizing: border-box;
 }
-
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -15,4 +15,17 @@ const router = new VueRouter({
   routes
 })
 
+//挂在路由守卫
+router.beforeEach((to, from, next) => {
+  //to将要访问的路径
+  //from 从哪个路径跳转来的
+  //next 是一个函数，表示放行
+  //  next()放行，next('/login')跳转到login页面
+  if(to.path === '/login') return next()
+  //获取token
+  const tonkenstr = window.sessionStorage.getItem('token')
+  if(!tonkenstr) return next('/login')
+  next()
+})
+
 export default router
